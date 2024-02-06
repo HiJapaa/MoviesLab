c22b0ed84f9916fe03863bb91b5e6c4bdb4fb70e
HiJapaa <pkenohacker@hotmail.com>
2024-02-06 18:59:43 -0400

    ADD

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     <nav id="navbar">
       <h2>
         <Link to="/">
-          <BiCameraMovie /> MoviesLib
+          <BiCameraMovie /> MoviesLab
         </Link>
       </h2>
       <form onSubmit={handleSubmit}>
